ecfeb39818984ebfcaabb4b3aa4fc2e56f392d20
Pra_Kas <prakash63690@gmail.com>
2024-08-30 08:56:44 +0530

    Empty commit

--- config/dataBase.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-const connection = mongoose.createConnection('mongodb://localhost:27017/QuizToPia').on('open', () => {
-    console.log("Database connected")
-}).on('error', () => {
-    console.log("Database not connected")
-})
-
-module.exports = connection
--- router/authRouter.js
+++
@@ -1,6 +0,0 @@
-const route = require('express').Router()
-const authController = require('../controller/authController')
-
-route.post('/addUser',authController.addUserController)
-
-module.exports = route
